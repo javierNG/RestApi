2194711ee883ebb51f6c4e4e32b5028fcea3f46e
jav nug <javier.knilb@gmail.com>
2019-06-27 21:37:24 -0500

    Initializintg test repository

---
+++ README.md
@@ -0,0 +1,7 @@
+# RESTApi
+
+
+http://localhost:8080/swagger-ui.html
+
+
+`./mvnw compile flyway:migrate`
---
+++ src/main/java/io/javng/spring/restapi/RestApi.java
@@ -0,0 +1,14 @@
+package io.javng.spring.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi {
+
+
+    public static void main(String[] args){
+        SpringApplication.run(RestApi.class, args);
+    }
+
+}
